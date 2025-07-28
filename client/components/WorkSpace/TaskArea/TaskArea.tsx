77034b31a9ef2f21039522177a2173f82d9c4eff
Aaroh Sinha <aarohsinha.programming@gmail.com>
2025-07-28 22:44:09 +0530

    FIX FRONTEND 3

--- client/components/WorkSpace/TaskArea/TaskArea.tsx
+++ client/components/WorkSpace/TaskArea/TaskArea.tsx
@@ -71,10 +71,8 @@ const TaskArea:React.FC<TaskAreaProp> = ({taskReload, setShowOverlay, setTaskRel
   }, [taskReload])
 
   return (
-    <div className="w-full flex justify-center py-4 bg-[#fdfdfd]">
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-4 w-full max-w-screen-xl">
-
-
+    <div className="w-full flex justify-center  py-4 bg-[#fdfdfd]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[2.2em]">
         {tasks.map((task, index) => (
           
           <TaskBox
