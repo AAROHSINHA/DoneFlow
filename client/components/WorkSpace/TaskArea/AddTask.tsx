9d098f1b50fe95bb4b78c7b7d76f6065f5af9f0a
Aaroh Sinha <aarohsinha.programming@gmail.com>
2025-07-30 19:59:18 +0530

    WORKSPACE

--- client/components/WorkSpace/TaskArea/AddTask.tsx
+++ client/components/WorkSpace/TaskArea/AddTask.tsx
@@ -13,15 +13,11 @@ export default function AddTask() {
   }
 
   return (
-    <div className="relative inline-block hover:cursor-pointer hover:z-50 transition transform duration-300 bg-purple-100 " onClick={handleAddTask}>
-    <div className={`w-full max-w-[26rem] h-full
- bg-gradient-to-br rounded-2xl p-6 flex flex-col justify-between relative overflow-hidden mb-[3em] transition-transform duration-200`}>
-
-     <Plus className="w-12 h-12 text-gray-400 group-hover:text-gray-600 group-hover:scale-110 transition-all duration-200" />
-
- </div>
- </div>
-  
- 
+    <button
+      onClick={handleAddTask}
+      className=" bg-gray-50 hover:bg-gray-100 border border-gray-200 rounded-2xl shadow-lg hover:shadow-md transition-all duration-200 flex items-center justify-center group hover:cursor-pointer"
+    >
+      <Plus className="w-12 h-12 text-gray-400 group-hover:text-gray-600 group-hover:scale-110 transition-all duration-200" />
+    </button>
   )
 }
