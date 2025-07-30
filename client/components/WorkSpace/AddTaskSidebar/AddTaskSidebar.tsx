3b3fdbe70e66275c4a1847194ecc3c341b8f3237
Aaroh Sinha <aarohsinha.programming@gmail.com>
2025-07-30 15:20:26 +0530

    ADD TASK SIDEBAR 1

--- client/components/WorkSpace/AddTaskSidebar/AddTaskSidebar.tsx
+++ client/components/WorkSpace/AddTaskSidebar/AddTaskSidebar.tsx
@@ -44,7 +44,7 @@ export default function AddTaskSidebar({ isOpen, onClose, setTaskReload, updateT
       }}
       >
       <div
-        className={`fixed right-0 top-0 h-full w-full sm:w-4/5 md:w-2/5 bg-white shadow-2xl transform transition-transform duration-300 ease-in-out z-50 ${
+        className={`fixed right-0 top-0 h-full w-full sm:w-4/5 md:w-2/5 bg-white shadow-2xl transform transition-transform duration-300 ease-in-out z-50 overflow-y-auto ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
